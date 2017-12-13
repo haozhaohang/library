b93d6aa53edc4e2e089e388897d2e7c00ac961a8
haozhaohang <745184644@qq.com>
2017-12-13 15:57:08 +0800

    :sparkles: add react-thunk

--- webpack react配置/lessons3/src/main/store.js
+++ webpack react配置/lessons3/src/main/store.js
@@ -1,9 +1,14 @@
-import { combineReducers, createStore  } from 'redux'
+import { combineReducers, createStore, applyMiddleware  } from 'redux'
+import thunk from 'redux-thunk'
 
-import rootReducers from '../reducers'
+import rootReducers from '../reduces'
+
+const middleware = [thunk]
 
 const store = createStore(
-    combineReducers(rootReducers)
+    combineReducers(rootReducers),
+    {},
+    applyMiddleware(...middleware)
 )
 
 export default store
